0894e3994fcd6e9eebe3945129d925440faa2fee
diwayou <diwayou@qq.com>
2016-03-16 14:56:16 +0800

    refactor dispatch

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/dispatch/DisruptorDispatchMessageService.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/dispatch/DisruptorDispatchMessageService.java
@@ -101,7 +101,7 @@ public class DisruptorDispatchMessageService {
 
         this.defaultMessageStore.getStoreStatsService().setDispatchMaxBuffer(this.ringBuffer.getBufferSize() - this.ringBuffer.remainingCapacity());
 
-        if (log.isWarnEnabled() && this.ringBuffer.remainingCapacity() < 100) {
+        if (log.isInfoEnabled() && this.ringBuffer.remainingCapacity() < 100) {
             log.info("RingBuffer remainingCapacity" + " < 100");
         }
     }
